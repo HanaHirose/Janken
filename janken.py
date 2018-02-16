d346e745a74427f10f4f34bd7537d663da9da29f
HanaHirose <kuu103tosaka007@gmail.com>
2018-02-16 14:35:17 +0000

    give an error if you choose an invalid number

--- janken.py
+++ janken.py
@@ -3,11 +3,21 @@ import getpass
 PersonA_win = 0
 PersonB_win = 0
 Aiko = 0
+Hands = [0,1,2]
 
 while True:
-    PersonA = int(getpass.getpass(prompt="Tomo, please choose your hand\n0 is scissors, 1 is rock, 2 is paper\n"))
-    PersonB = int(getpass.getpass(prompt="Hana, please choose your hand\n0 is scissors, 1 is rock, 2 is paper\n"))
-    
+    while True:
+        PersonA = int(getpass.getpass(prompt="Tomo, please choose your hand\n0 is scissors, 1 is rock, 2 is paper\n"))
+        if PersonA in Hands:
+            break
+        else:
+            print("You did not choose a valid hand! Please input your hand again!\n")
+    while True:
+        PersonB = int(getpass.getpass(prompt="Hana, please choose your hand\n0 is scissors, 1 is rock, 2 is paper\n"))
+        if PersonB in Hands:
+            break
+        else:
+            print("You did not choose a valid hand! Please input your hand again!\n")
     if PersonA == 0:
         print("Tomo played scissors\n")
     elif PersonA == 1:
