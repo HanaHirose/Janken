1f5c858fa530dd38d13f67489bcd8ee7074c73e6
HanaHirose <kuu103tosaka007@gmail.com>
2018-02-16 12:16:46 +0000

    use a better algorithm

--- janken.py
+++ janken.py
@@ -5,9 +5,7 @@ PersonB = int(random.random()*3)
 
 if PersonA == PersonB:
     print("Oh, it was Aiko")
-elif PersonA - PersonB == 1:
-    print("PersonA won")
-elif PersonA - PersonB == -2:
+elif (PersonA - PersonB)%3 == 1:
     print("PersonA won")
 else:
     print("PersonB won")
